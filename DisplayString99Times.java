45a3de38dc88a7b9dec0b4eb6a7c21d7d009ed59
Mary N <112522425+fuseboxradio@users.noreply.github.com>
2023-09-21 18:13:30 -0400

    Create DisplayString99Times.java

---
+++ DisplayString99Times.java
@@ -0,0 +1,9 @@
+public class MusicLoop {
+    public static void main(String[] args) {
+        //for loop
+        int i;
+        for (i = 1; i < 100; i ++) {
+            System.out.println("Wu-Tang Is For The Children! ");
+        }
+    }
+}
