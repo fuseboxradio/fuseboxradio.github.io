c34d803e2949d1c17d82e27efd1401d022f1f0dc
Mary N <112522425+fuseboxradio@users.noreply.github.com>
2023-09-21 18:15:04 -0400

    Create EvenNumbersTo99PartTwo.java

---
+++ EvenNumbersTo99PartTwo.java
@@ -0,0 +1,9 @@
+public class EvenNumbers {
+    public static void main(String[] args) {
+        //for loop
+        int i;
+        for (i = 0; i <= 99; i+=2) {
+            System.out.println(i + ", ");
+        }
+    }
+}
